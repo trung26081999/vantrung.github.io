8b643be960c92aa4c557f6742aa74093227f6999
trung26081999 <trung26081999@gmail.com>
2022-09-08 19:16:12 +0700

    First commit

---
+++ fe27-homework-van-trung/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { CardTitle } from './components/CardTitle';
+
+function App() {
+  return (
+    <div className="App">
+      <CardTitle/>
+    </div>
+  );
+}
+
+export default App;
